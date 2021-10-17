7c56081a84570c01f7edb16c68b248c95cefaf24
zach.burnaby <58493767+TheRedLancer@users.noreply.github.com>
2021-10-16 20:35:19 -0700

    update database dummy data

--- add-crap.sql
+++ add-crap.sql
@@ -1,9 +1,9 @@
 INSERT INTO LOCATION (lid, name, address, city, country)
     VALUES
-        (1, "pizza", "123 South", "Spokane", "US"),
-        (2, "tacos", "555 North", "Spokane", "US"),
-        (3, "burger", "983 West", "Seattle", "US"),
-        (4, "pizza", "123 South", "Los Angeles", "US");
+        (1, "pizza", "123 South", "Florence", "Italy"),
+        (2, "tacos", "555 North", "Florence", "Italy"),
+        (3, "burger", "983 West", "Florence", "Italy"),
+        (4, "bbq", "123 South", "Rio", "Brazil");
     
 INSERT INTO PROGRAM (pid, name, school, city, country)
     VALUES
@@ -12,18 +12,18 @@ INSERT INTO PROGRAM (pid, name, school, city, country)
 
 INSERT INTO USER (uid, name, sem_attend, program)
     VALUES
-        (7, "Zach", "Fall 21", "5"),
-        (8, "Ariana", "Spring 22", "5"),
-        (9, "Daniel", "Winter 18", "6"),
-        (10, "Maddie", "Spring 22", "6"),
-        (11, "Jaymin", "Summer 19", "5");
+        (7, "Zach", "Fall 21", "gu-in-florence"),
+        (8, "Ariana", "Spring 22", "gu-in-florence"),
+        (9, "Daniel", "Winter 18", "UW-in-Brazil"),
+        (10, "Maddie", "Spring 22", "UW-in-Brazil"),
+        (11, "Jaymin", "Summer 19", "gu-in-florence");
 
 INSERT INTO REVIEW (rid, user, date, location, text, stars, price)
     VALUES
-        (12, 7, 1634420735, 1, "terrible staff", 1, 3),
-        (13, 7, 1634420735, 2, "tasty taco", 5, 2),
-        (14, 8, 1653083135, 4, "good pizza", 4, 1),
-        (15, 9, 1516398335, 3, "pretty expensive", 3, 3),
-        (16, 10, 1652996735, 4, "fun atmosphere", 5, 3),
-        (17, 11, 1563572735, 3, "great cheeseburger", 4, 1);
+        (12, "Zach", 1634420735, "pizza", "terrible staff", 1, 3),
+        (13, "Zach", 1634420735, "tacos", "tasty taco", 5, 2),
+        (14, "Ariana", 1653083135, "burger", "good pizza", 4, 1),
+        (15, "Daniel", 1516398335, "bbq", "pretty expensive", 3, 3),
+        (16, "Maddie", 1652996735, "bbq", "fun atmosphere", 5, 3),
+        (17, "Jaymin", 1563572735, "burger", "great fries", 4, 1);
         
